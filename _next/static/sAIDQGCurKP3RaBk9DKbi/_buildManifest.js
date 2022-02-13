bd429f0ed775eee58ce6cc6fdb4f3e1b686818aa
AideTechBot <mandio0302@gmail.com>
2022-02-13 20:25:45 +0000

    Deploying to gh-pages from  @ d690b4dd7eb97313e5f64d40ad2fb91ac4c9bb72 🚀

--- _next/static/sAIDQGCurKP3RaBk9DKbi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/414-91ef8f21d7e13b7a.js","static/css/8ab991c358d80973.css","static/chunks/pages/index-f5b53a6210234485.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
